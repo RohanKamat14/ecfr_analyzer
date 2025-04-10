57016e4afdb8a8c11afef4905b27117711b551fb
RohanKamat14 <rohannkamat@gmail.com>
2025-04-10 05:37:54 -0500

    Published

--- app.py
+++ app.py
@@ -4,17 +4,29 @@ import requests
 
 app = Flask(__name__)
 
-search_suggestions = ["All"]
+search_suggestions = []
+results = []
 
+def fetch_agencies():
+    url = 'https://www.ecfr.gov/api/admin/v1/agencies.json'
+    response = requests.get(url)
+
+    # Ensure that the response is JSON and return the data
+    if response.status_code == 200:
+        return response.json()  # Parse JSON response
+    else:
+        print(f"Error fetching data: {response.status_code}")
+        return []
+    
+agencies_data = fetch_agencies()
 @app.route('/')
 def hello():
-    agencies_data = fetch_agencies()
     for x in agencies_data.values():
         for i in x:
             search_suggestions.append(i.get('name'))
     return render_template('index.html')
 
-results = []
+
 @app.route('/search')
 def search():
     query = request.args.get('q')
@@ -27,34 +39,18 @@ def search():
             results.clear()
         else:
             if query or q1 and q2:
-                # Perform search logic here (e.g., query a database)
                 if category == 'word_count':
-                    if query == "All":
-                        for x in search_suggestions[1:]:
-                            results.append(f"The {x} has a word count of " + str(find_agency_word_count(x)))
-                    else: 
-                        results.append(f"The {query} has a word count of " + str(find_agency_word_count(query)))
+                    results.append(f"The {query} has a word count of " + str(find_agency_word_count(query)))
                 elif category == 'historical':
                     results.append(f"There were " + str(query_historical(q1, q2)) + " historical changes between the dates " + str(q1) + " and " + str(q2))
                 elif category == 'corrections':
                     results.append(f"There have been " + str(query_corrections(query)) + " corrections on title number " + str(query))
     
-    return render_template('index.html', query=query, results=results)
+    return render_template('index.html', query=query, results=results, category=category)
 
 @app.route('/search_suggestions')
 def search_suggestions_route():
     return jsonify(search_suggestions)
-
-def fetch_agencies():
-    url = 'https://www.ecfr.gov/api/admin/v1/agencies.json'
-    response = requests.get(url)
-
-    # Ensure that the response is JSON and return the data
-    if response.status_code == 200:
-        return response.json()  # Parse JSON response
-    else:
-        print(f"Error fetching data: {response.status_code}")
-        return []
     
 def Get_Size(response):
     """
@@ -68,7 +64,6 @@ def Get_Size(response):
 # Calculates the word count for the given federal agency
 def find_agency_word_count(name):
     word_count = 0
-    agencies_data = fetch_agencies()
     # Ensure we are handling the correct data structure (list of agencies)
     for agency in agencies_data.values():
         for x in agency:
@@ -91,9 +86,10 @@ def query_corrections(title):
     count = 0
     url = f'https://www.ecfr.gov/api/admin/v1/corrections/title/{title}.json'
     response = requests.get(url)
-    for x in response.json().values():
-        for i in x:
-            count += 1
+    if response:
+        for x in response.json().values():
+            for i in x:
+                count += 1
     return count
 
 def query_historical(startDate, endDate):
